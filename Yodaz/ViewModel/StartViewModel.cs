eb89bb1e6145a7e100232b64f1eb97e241be83af
RiaLia <rialia@live.se>
2018-12-07 14:07:43 +0100

    Adding startPage with an entry and button, connected to startViewModel

---
+++ Yodaz/ViewModel/StartViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Yodaz.ViewModel
+{
+    public class StartViewModel
+    {
+        public ICommand StartCommand { get; set; }
+
+        public StartViewModel()
+        {
+            StartCommand = new Command(
+            execute: () => Console.WriteLine("Fetch user input from entry.."));
+        }
+    }
+}

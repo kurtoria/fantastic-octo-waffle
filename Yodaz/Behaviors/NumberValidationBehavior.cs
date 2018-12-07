ee26e376c583e51bea1e8b75d465fa6108050439
kurtoria <victoriagronqvist@gmail.com>
2018-12-07 15:07:37 +0100

    Number validation for user input added

---
+++ Yodaz/Behaviors/NumberValidationBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Yodaz.Behaviors
+{
+    public class NumberValidationBehavior : Behavior<Entry>
+    {
+        public NumberValidationBehavior()
+        {
+        }
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            int result;
+            bool isValid = int.TryParse(e.NewTextValue, out result);
+            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
+        }
+    }
+}

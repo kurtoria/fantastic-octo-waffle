f5629426f91ad204f74fc866df9173fcb9ba4d81
emmapersson <emma.lp85@gmail.com>
2018-12-10 10:38:41 +0100

    Fixed Trivia

---
+++ Yodaz/Model/Trivia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Yodaz.Model
+{
+    public class Trivia
+    {
+        public string Question { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string IncorrectAnswer { get; set; }
+
+        public Trivia(string question, string correctAnswer, string incorrectAnswer)
+        {
+            this.Question = question;
+            this.CorrectAnswer = correctAnswer;
+            this.IncorrectAnswer = incorrectAnswer;
+        }
+    }
+}

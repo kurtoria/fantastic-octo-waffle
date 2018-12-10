c93866600174ee4ad5f5f4cf20905a68e5b420f5
emmapersson <emma.lp85@gmail.com>
2018-12-10 12:47:39 +0100

    MaxLengthValidationBehavior

---
+++ Yodaz/Behaviors/MaxLengthValidationBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Linq;
+namespace Yodaz.Behaviors
+{
+    public class MaxLengthValidationBehavior : Behavior<Entry>
+    {
+
+        public static BindableProperty MaxLengthProperty = BindableProperty.Create("MaxLength", typeof(int), typeof(MaxLengthValidationBehavior), 0);
+        
+        public int MaxLength
+        {
+            get 
+            { 
+                return (int)GetValue(MaxLengthProperty);
+            }
+            set
+            {
+                SetValue(MaxLengthProperty, value);
+            }
+        }
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += BindableTextChanged;
+        }
+
+        private void BindableTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (e.NewTextValue.Length >= MaxLength)
+            {
+                ((Entry)sender).Text = e.NewTextValue.Substring(0, MaxLength);
+            }
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= BindableTextChanged;
+        }
+    }
+}

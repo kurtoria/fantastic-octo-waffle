7e3769a1bd9659ddf254cd7dca3626364d79ecfc
kurtoria <victoriagronqvist@gmail.com>
2018-12-11 15:26:32 +0100

    Added functionality to async method

--- Yodaz/Services/HTTPWebRequest.cs
+++ Yodaz/Services/HTTPWebRequest.cs
@@ -15,9 +15,9 @@ namespace Yodaz.Services
         public static List<Trivia> Trivias = new List<Trivia>();
         public string Url = "";
 
-        private static string GetUrl(int nr)
+        private static string GetUrl(int number)
         {
-            return "https://opentdb.com/api.php?amount=" + nr.ToString() + "&type=boolean";
+            return "https://opentdb.com/api.php?amount=" + number.ToString() + "&type=boolean";
         }
 
         //public static void GetTrivia(int number)
@@ -52,49 +52,54 @@ namespace Yodaz.Services
         //}
 
 
-        public static void GetTrivia(int number)
+        public static async Task GetTriviaAsync(int number)
         {
-            //HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create(string.Format(GetUrl(number)));
-            //WebReq.Method = "GET";
+            HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create(string.Format(GetUrl(number)));
+            WebReq.Method = "GET";
 
-            //HttpWebResponse WebResp = (HttpWebResponse)WebReq.GetResponse();
+            //asynca denna?
+            //HttpWebResponse WebResp = await Task.Run(() => (HttpWebResponse)WebReq.GetResponse());
+            HttpWebResponse WebResp = (HttpWebResponse)WebReq.GetResponse();
 
-            //Console.WriteLine("---------------------------");
-            //Console.WriteLine(WebResp.StatusCode);
-            //Console.WriteLine(WebResp.Server);
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(WebResp.StatusCode);
+            Console.WriteLine(WebResp.Server);
 
-            //string jsonString;
-            //using (Stream stream = WebResp.GetResponseStream())
-            //{
-            //    StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
-            //    jsonString = reader.ReadToEnd();
-            //}
-            //string trimStartString = jsonString.TrimStart('{', '"', 'r', 'e', 's', 'p', 'o', 'n', 's', 'e', '_', 'c', 'o', 'd', 'e', ':', '0', ',', 'u', 'l', 't', 's');
-            //string trimEndString = trimStartString.TrimEnd('}');
-
-            //try
-            //{
-            //    Trivias = JsonConvert.DeserializeObject<List<Trivia>>(trimEndString);
-
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine("\n");
-            //    Console.WriteLine("ISSUE: " + e);
-            //}
-
-            //return Trivias;
-
-
-
-
-
-            HttpClient client = new HttpClient();
-            string json = client.GetStringAsync(GetUrl(number)).Result;
-            string trimStartString = json.TrimStart('{', '"', 'r', 'e', 's', 'p', 'o', 'n', 's', 'e', '_', 'c', 'o', 'd', 'e', ':', '0', ',', 'u', 'l', 't', 's');
+            string jsonString;
+            using (Stream stream = WebResp.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
+                jsonString = reader.ReadToEnd();
+            }
+            string trimStartString = jsonString.TrimStart('{', '"', 'r', 'e', 's', 'p', 'o', 'n', 's', 'e', '_', 'c', 'o', 'd', 'e', ':', '0', ',', 'u', 'l', 't', 's');
             string trimEndString = trimStartString.TrimEnd('}');
 
-            Trivias = JsonConvert.DeserializeObject<List<Trivia>>(trimEndString);
+
+            try
+            {
+                //Trivias = await Task.Run(() => JsonConvert.DeserializeObject<List<Trivia>>(trimEndString));
+                Trivias = JsonConvert.DeserializeObject<List<Trivia>>(trimEndString);
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Trivia count from QuizViewModel: " + HTTPWebRequest.Trivias.Count);
+                Console.WriteLine("--------------------------------------");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("ISSUE: " + e);
+            }
+
+
+
+
+
+            //HttpClient client = new HttpClient();
+            //string json = client.GetStringAsync("https://opentdb.com/api.php?amount=" + number.ToString() + "&type=boolean").Result;
+            //string trimStartString = json.TrimStart('{', '"', 'r', 'e', 's', 'p', 'o', 'n', 's', 'e', '_', 'c', 'o', 'd', 'e', ':', '0', ',', 'u', 'l', 't', 's');
+            //string trimEndString = trimStartString.TrimEnd('}');
+
+            //Trivias = JsonConvert.DeserializeObject<List<Trivia>>(trimEndString);
         }
     }
 }

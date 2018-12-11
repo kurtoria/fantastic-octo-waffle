f9439dc9794705c749f229a1e89ed02f73d4631e
kurtoria <victoriagronqvist@gmail.com>
2018-12-11 13:01:07 +0100

    fetch updated to work async

--- Yodaz/Services/HTTPWebRequest.cs
+++ Yodaz/Services/HTTPWebRequest.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using Yodaz.Model;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace Yodaz.Services
 {
@@ -19,39 +20,7 @@ namespace Yodaz.Services
             return "https://opentdb.com/api.php?amount=" + nr.ToString() + "&type=boolean";
         }
 
-        public static void GetTrivia(int number)
-        {
-            HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create(string.Format(GetUrl(number)));
-            WebReq.Method = "GET";
-
-            HttpWebResponse WebResp = (HttpWebResponse)WebReq.GetResponse();
-
-            Console.WriteLine("---------------------------");
-            Console.WriteLine(WebResp.StatusCode);
-            Console.WriteLine(WebResp.Server);
-
-            string jsonString;
-            using (Stream stream = WebResp.GetResponseStream())
-            {
-                StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
-                jsonString = reader.ReadToEnd();
-            }
-            string trimStartString = jsonString.TrimStart('{', '"', 'r', 'e', 's', 'p', 'o', 'n', 's', 'e', '_', 'c', 'o', 'd', 'e', ':', '0', ',', 'u', 'l', 't', 's');
-            string trimEndString = trimStartString.TrimEnd('}');
-
-            try
-            {
-                Trivias = JsonConvert.DeserializeObject<List<Trivia>>(trimEndString);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("\n");
-                Console.WriteLine("ISSUE: " + e);
-            }
-        }
-
-
-        //public static void GetTrivia2(string number)
+        //public static void GetTrivia(int number)
         //{
         //    HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create(string.Format(GetUrl(number)));
         //    WebReq.Method = "GET";
@@ -68,21 +37,66 @@ namespace Yodaz.Services
         //        StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
         //        jsonString = reader.ReadToEnd();
         //    }
-        //    AsyncDes(jsonString);
-        //}
-
-
-        //public static async Task<List<Trivia>> AsyncDes(string jsonString)
-        //{
         //    string trimStartString = jsonString.TrimStart('{', '"', 'r', 'e', 's', 'p', 'o', 'n', 's', 'e', '_', 'c', 'o', 'd', 'e', ':', '0', ',', 'u', 'l', 't', 's');
         //    string trimEndString = trimStartString.TrimEnd('}');
 
-        //    return Trivias = JsonConvert.DeserializeObject<List<Trivia>>(trimEndString);
-        //    //catch (Exception e)
-        //    //{
-        //    //    Console.WriteLine("\n");
-        //    //    Console.WriteLine("ISSUE: " + e);
-        //    //}
+        //    try
+        //    {
+        //        Trivias = JsonConvert.DeserializeObject<List<Trivia>>(trimEndString);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("\n");
+        //        Console.WriteLine("ISSUE: " + e);
+        //    }
         //}
+
+
+        public static void GetTrivia(int number)
+        {
+            //HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create(string.Format(GetUrl(number)));
+            //WebReq.Method = "GET";
+
+            //HttpWebResponse WebResp = (HttpWebResponse)WebReq.GetResponse();
+
+            //Console.WriteLine("---------------------------");
+            //Console.WriteLine(WebResp.StatusCode);
+            //Console.WriteLine(WebResp.Server);
+
+            //string jsonString;
+            //using (Stream stream = WebResp.GetResponseStream())
+            //{
+            //    StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
+            //    jsonString = reader.ReadToEnd();
+            //}
+            //string trimStartString = jsonString.TrimStart('{', '"', 'r', 'e', 's', 'p', 'o', 'n', 's', 'e', '_', 'c', 'o', 'd', 'e', ':', '0', ',', 'u', 'l', 't', 's');
+            //string trimEndString = trimStartString.TrimEnd('}');
+
+            //try
+            //{
+            //    Trivias = JsonConvert.DeserializeObject<List<Trivia>>(trimEndString);
+
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine("ISSUE: " + e);
+            //}
+
+            //return Trivias;
+
+
+
+
+
+            HttpClient client = new HttpClient();
+            // read the json into a string
+            // string could potentially be very large and cause memory problems
+            string json = client.GetStringAsync(GetUrl(number)).Result;
+            string trimStartString = json.TrimStart('{', '"', 'r', 'e', 's', 'p', 'o', 'n', 's', 'e', '_', 'c', 'o', 'd', 'e', ':', '0', ',', 'u', 'l', 't', 's');
+            string trimEndString = trimStartString.TrimEnd('}');
+
+            Trivias = JsonConvert.DeserializeObject<List<Trivia>>(trimEndString);
+        }
     }
 }

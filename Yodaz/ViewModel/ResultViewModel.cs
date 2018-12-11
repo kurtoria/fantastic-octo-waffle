d8db0172893766caa9faaa19035754cbca6c6e83
emmapersson <emma.lp85@gmail.com>
2018-12-11 13:35:16 +0100

    Properties for result and PlayCommand

--- Yodaz/ViewModel/ResultViewModel.cs
+++ Yodaz/ViewModel/ResultViewModel.cs
@@ -1,19 +1,35 @@
 ﻿using System;
 
 using Xamarin.Forms;
+using Yodaz.Model;
+using System.Windows.Input;
+using Yodaz.Navigation;
+using System.Threading.Tasks;
 
 namespace Yodaz.ViewModel
 {
-    public class ResultViewModel : ContentPage
+    public class ResultViewModel
     {
+        INavigationService _navigationService;
+        public string FinalScore { get; set; }
+
+        public string ResultText { get; set; }
+
+        public ICommand PlayCommand { get; private set; }
+
         public ResultViewModel()
         {
-            Content = new StackLayout
-            {
-                Children = {
-                    new Label { Text = "Hello ContentPage" }
-                }
-            };
+            _navigationService = App.NavigationService;
+            FinalScore = $"{User.Result}/{User.Input}";
+
+            PlayCommand = new Command(
+               execute: async () => await PlayAgain(),
+               canExecute: () => { return true; });
+        }
+
+        public async Task PlayAgain()
+        {
+            await _navigationService.NavigateAsync("StartPage");
         }
     }
 }

6fa5308922f55043622d28836336eae05479278d
emmapersson <emma.lp85@gmail.com>
2018-12-11 11:42:20 +0100

    Added ResultViewModel, ResultPage and Result property

---
+++ Yodaz/ViewModel/ResultViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Yodaz.ViewModel
+{
+    public class ResultViewModel : ContentPage
+    {
+        public ResultViewModel()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
